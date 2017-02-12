2322e4de0ad51527d00c0dfc897f6edf2f7a185e
Aditya Malusare <aditya.malusare@gmail.com>
2017-02-13 01:12:22 +0530

    Continue outline of the code
    No internet :'(

--- hardware.py
+++ hardware.py
@@ -1,10 +1,20 @@
-
+import #communication stuff
 
 def sense():
 	"Return 2-list of sensor difference at [L, R]"
+	#return string: S1, S2, S3 
+	sensors = client("0").split(",")
+	return [sensors[1] - sensors[2], sensors[3] - sensors[2]]
 
-def pulse():
+def pulse(inp):
 	"Send pulse to engine"
+	client(str(inp))
 
-def rpulse():
+def rpulse(inp):
 	"Execute pulse in reverse"
+	inp.reverse()
+	pulse(inp)
+
+def client(command):
+	"Send HTTP or equivalent request to Arduino
+	return out
